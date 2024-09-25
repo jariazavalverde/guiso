26ab2a28fabf6430904a3cb8a2720aabdde94dbe
jariazavalverde <riazavalverde@gmail.com>
2024-09-25 02:14:30 +0200

    added get method

--- src/graph.rs
+++ src/graph.rs
@@ -3,7 +3,24 @@ pub struct Graph<T> {
     vertices: usize,
 }
 
-impl<T> Graph<T> {}
+impl<T> Graph<T> {
+    /// Returns the ij-entry of the adjacency matrix.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use guiso::graph::Graph;
+    ///
+    /// let g: Graph<u8> = Graph::from([[1,0,0],[0,1,0],[0,0,1]]);
+    ///
+    /// assert_eq!(1, *g.get(0, 0));
+    /// assert_eq!(1, *g.get(1, 1));
+    /// assert_eq!(0, *g.get(2, 0));
+    /// ```
+    pub fn get(&self, row: usize, col: usize) -> &T {
+        &self.matrix[row * self.vertices + col]
+    }
+}
 
 impl<const V: usize, T> From<[[T; V]; V]> for Graph<T>
 where
@@ -16,11 +33,11 @@ where
     /// ```
     /// use guiso::graph::Graph;
     ///
-    /// let g: Graph<u8> = Graph::from([[1,0,0],[0,1,0],[0,0,0]]);
+    /// let g: Graph<u8> = Graph::from([[1,0,0],[0,1,0],[0,0,1]]);
     /// ```
     fn from(arr: [[T; V]; V]) -> Self {
         let mut matrix: Vec<T> = Vec::new();
-        for i in 1..V {
+        for i in 0..V {
             matrix.extend_from_slice(&arr[i]);
         }
         Graph {
